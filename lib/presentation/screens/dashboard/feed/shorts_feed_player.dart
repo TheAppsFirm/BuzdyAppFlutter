c9c0a76317b38bb78e3eef654a4f416533025e58
Zia Shahid <ziagaggoo@gmail.com>
2025-06-15 01:50:56 +0400

    Fix share plus usage

--- lib/presentation/screens/dashboard/feed/shorts_feed_player.dart
+++ lib/presentation/screens/dashboard/feed/shorts_feed_player.dart
@@ -29,11 +29,10 @@ class _ShortsFeedPlayerState extends State<ShortsFeedPlayer> {
     final url = 'https://youtu.be/$id';
 
     try {
-      final params = ShareParams(
-        text: url,
+      await Share.share(
+        url,
         subject: 'Check out this video',
       );
-      await SharePlus.instance.share(params);
     } catch (e) {
       EasyLoading.showError('Sharing not available');
     }
